b708a44adb00a4e3ebe2705db8531ab6222d5664
Adam <adam@tealemonade.us>
2018-05-23 14:49:11 -0600

    GDPR5

--- wp-content/themes/parish/js/navigation.js
+++ wp-content/themes/parish/js/navigation.js
@@ -43,27 +43,36 @@
 	};
 } )();
 
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
 function getCookie(name) {
-	    var nameEQ = name + "=";
-	    var ca = document.cookie.split(';');
-	    for(var i=0;i < ca.length;i++) {
-	        var c = ca[i];
-	        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-	        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-	    }
-	    return null;
-	}
-	function eraseCookie(name) {   
-	    document.cookie = name+'=; Max-Age=-99999999;';  
-	}
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {   
+    document.cookie = name+'=; Max-Age=-99999999;';  
+}
 
-	var x = getCookie('gdpraccept');
-	if (x) {
-	    //jQuery('.gdpr-popup').hide();
-	}
-	else{
-		jQuery('.gdpr-popup').show();
-	}
+var x = getCookie('gdpraccept');
+if (x) {
+    //jQuery('.gdpr-popup').hide();
+}
+else{
+	jQuery('.gdpr-popup').show();
+}
 	jQuery(document).ready(function(){
 	  $url = window.location.href;
 	  $n = $url.indexOf("#faq");
