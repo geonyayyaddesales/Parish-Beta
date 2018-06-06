d8d50de6adf51e9f203a4a80dfeabf6b30d1b208
Adam <adam@tealemonade.us>
2018-06-06 15:50:25 -0600

    GDPR Update

--- wp-content/themes/parish/sidebar.php
+++ wp-content/themes/parish/sidebar.php
@@ -22,7 +22,7 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
                 
                 <input class="button" id="mc-embedded-subscribe" type="submit" name="subscribe" value="Sign up">
 <a href="http://thetablet.org/emailsignup/" target="_blank" >See sample</a>
-                
+                <span class="gdpr">I'm aware that my information is being collected for marketing purposes. <a href="/privacy-policy/">More info</a></span>
                 <div class="clear"></div>
                 
                 <div class="clear" id="mce-responses"></div>
