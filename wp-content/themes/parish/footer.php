68cc2cfe3ebd218dc93261c9a07347df5dc57eca
Adam <adam@tealemonade.us>
2018-07-10 14:54:54 -0600

    Privacy Policy

--- wp-content/themes/parish/footer.php
+++ wp-content/themes/parish/footer.php
@@ -25,7 +25,7 @@
                 	<?php echo do_shortcode("[connect]"); ?>
             
             		<div class="clear"></div>
-                    <p class="sm">&copy; <?php echo date("Y");?> <a href="http://desalesmedia.org/" title="DeSales Media Group, Inc." target="_blank">DeSales Media Group, Inc.</a> All Rights Reserved. Website by <a href="http://345design.com/" title="345 Design" target="_blank">345 Design.</a> </p>
+                    <p class="sm">&copy; <?php echo date("Y");?> <a href="http://desalesmedia.org/" title="DeSales Media Group, Inc." target="_blank">DeSales Media Group, Inc.</a> All Rights Reserved. Website by <a href="http://345design.com/" title="345 Design" target="_blank">345 Design.</a> | <a href="/privacy-policy/">Privacy Policy</a></p>
                     <p class="sm">A part of the <a href="http://dioceseofbrooklyn.org/" title="The Diocese of Brooklyn" target="_blank">Diocese of Brooklyn</a>, serving Brooklyn and Queens</p>
 	            	<div class="clear"></div>
                     
